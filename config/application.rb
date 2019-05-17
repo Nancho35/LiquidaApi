681b0354da03e9ede9a9171db8fd690083846927
Hernan <hernandario35@gmail.com>
2019-05-17 17:35:20 -0500

    otro

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,12 @@ Bundler.require(*Rails.groups)
 module LiquidaApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.time_zone = 'Lima'
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
-end
-end
 end
