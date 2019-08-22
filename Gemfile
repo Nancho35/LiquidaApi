9dbd9081b17a4bdc5344c5224479ae05c5ce1685
Hernan <hernandario35@gmail.com>
2019-08-22 17:29:41 -0500

    conexion a Oracle

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.5'
+gem "activerecord-oracle_enhanced-adapter"
+gem 'ruby-oci8', '~> 2.2', '>= 2.2.2'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
 gem 'pg'
--- app/models/liquidacion.rb
+++ app/models/liquidacion.rb
@@ -1,6 +1,6 @@
 class Liquidacion < ApplicationRecord
-  
-    def self.to_csv(options = {})
+
+    def self.to_csv(options = {encoding: 'UTF-8'})
       CSV.generate(options) do |csv|
         csv << column_names
         all.each do |liquidacions|
@@ -10,3 +10,4 @@ class Liquidacion < ApplicationRecord
     end
 
 end
+  
